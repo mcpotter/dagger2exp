575675287321fb5d506de44e3544fc4ecc1d3353
Matthew Potter <matthew@jtribe.com.au>
2016-12-15 10:28:29 +1100

    test push

--- app/src/main/java/au/com/jtribe/dagger2experiment/activity/ActivityComponent.java
+++ app/src/main/java/au/com/jtribe/dagger2experiment/activity/ActivityComponent.java
@@ -6,7 +6,7 @@ import au.com.jtribe.dagger2experiment.ApplicationComponent;
 import dagger.Component;
 
 /**
- * Created by matthewpotter on 9/12/2016.
+ * Created by matt on 9/12/2016.
  */
 @Component(dependencies = ApplicationComponent.class, modules = ActivityModule.class)
 @PerActivity
